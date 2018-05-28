05c8b422d463a47c9a4f0b30493b7de02fab95a9
Jon Perkins <captainperk81@gmail.com>
2018-05-28 18:46:54 -0400

    Implementing the Cart Controller

---
+++ SportsStore/Controllers/CartController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System.Linq;
+
+namespace SportsStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository _repository;
+
+        public CartController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = _repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            if (null != product)
+            {
+                Cart cart = GetCart();
+                cart.AddItem(product, 1);
+                SaveCart(cart);
+            }
+
+            return RedirectToAction("Index", new {returnUrl});
+        }
+
+        public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = _repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            if (null != product)
+            {
+                Cart cart = GetCart();
+                cart.RemoveLine(product);
+                SaveCart(cart);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart() => HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+
+        private void SaveCart(Cart cart) => HttpContext.Session.SetJson("Cart", cart);
+    }
+}
