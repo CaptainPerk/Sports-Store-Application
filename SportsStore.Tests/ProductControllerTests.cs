8accc6d2682bfb64e0ce2524dfca938b081bbf2e
Jon Perkins <captainperk81@gmail.com>
2018-05-28 15:50:20 -0400

    Correcting the Page Count

--- SportsStore.Tests/ProductControllerTests.cs
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using SportsStore.Controllers;
 using SportsStore.Models;
 using SportsStore.Models.ViewModels;
+using System;
 using System.Linq;
 using Xunit;
 
@@ -86,7 +88,38 @@ namespace SportsStore.Tests
             //Assert
             Assert.Equal(2, result.Length);
             Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
-            Assert.True(result[0].Name == "P4" && result[0].Category == "Cat2");
+            Assert.True(result[1].Name == "P4" && result[1].Category == "Cat2");
+        }
+
+        [Fact]
+        public void Generate_Category_Specific_Product_Count()
+        {
+            //Arrange
+            var _mockProductRepository = new Mock<IProductRepository>();
+            _mockProductRepository.Setup(repository => repository.Products).Returns(new[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category = "Cat1" },
+                new Product { ProductID = 2, Name = "P2", Category = "Cat2" },
+                new Product { ProductID = 3, Name = "P3", Category = "Cat1" },
+                new Product { ProductID = 4, Name = "P4", Category = "Cat2" },
+                new Product { ProductID = 5, Name = "P5", Category = "Cat3" }
+            }.AsQueryable());
+
+            var productController = new ProductController(_mockProductRepository.Object) { PageSize = 3 };
+
+            Func<ViewResult, ProductsListViewModel> GetModel = result => result?.ViewData?.Model as ProductsListViewModel;
+
+            //Act
+            int? result1 = GetModel(productController.List("Cat1"))?.PagingInfo.TotalItems;
+            int? result2 = GetModel(productController.List("Cat2"))?.PagingInfo.TotalItems;
+            int? result3 = GetModel(productController.List("Cat3"))?.PagingInfo.TotalItems;
+            int? result4 = GetModel(productController.List(null))?.PagingInfo.TotalItems;
+
+            //Assert
+            Assert.Equal(2, result1);
+            Assert.Equal(2, result2);
+            Assert.Equal(1, result3);
+            Assert.Equal(5, result4);
         }
     }
 }
